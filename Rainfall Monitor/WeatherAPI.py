0c782687d5dbf05b149bf4edbce8baed08a2a2a1
Chris Hennes <chennes@pioneerlibrarysystem.org>
2022-03-14 16:42:14 -0500

    Add new project for using the weather.gov API

---
+++ Rainfall Monitor/WeatherAPI.py
@@ -0,0 +1,48 @@
+# Weather API: A simple app for learning Python
+#
+# This program downloads the latest rainfall data for Norman, OK and displays the seven-day 
+# total accumulation.
+#
+# Copyright 2022 Pioneer Library System
+#
+# LICENSE: BSD 3-Clause
+#
+# Redistribution and use in source and binary forms, with or without modification, are 
+# permitted provided that the following conditions are met:
+#
+#   1. Redistributions of source code must retain the above copyright notice, this list 
+#      of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright notice, this 
+#      list of conditions and the following disclaimer in the documentation and/or other 
+#      materials provided with the distribution.
+#   3. Neither the name of the copyright holder nor the names of its contributors may be 
+#      used to endorse or promote products derived from this software without specific 
+#      prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+# THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import requests
+import json
+
+from typing import List
+
+LATITUDE = 35.22
+LONGITUDE = -97.44
+WEATHER_API_START_URL = f"https://api.weather.gov/points/{LATITUDE},{LONGITUDE}"
+
+if __name__ == "__main__":
+    r = requests.get(WEATHER_API_START_URL)
+    results = json.loads(r.text)
+
+    print (json.dumps(results, indent=4))
+
+    # At this point results is a dictionary with our point information: now we need to extract
+    # the data we want and fetch the forecast...
